c983000e0a1b808c91df050a8c1fabcdb36bd2df
ChiefMasterkim <88433943+ChiefMasterkim@users.noreply.github.com>
2022-10-01 22:23:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+function light(){
+    document.querySelector('.header ').classList.toggle('active');
+    document.getElementById('light').style.display="none";
+    document.getElementById('dark').style.display="block";
+    document.querySelector('.body ').classList.toggle('active');
+    document.querySelector('#content ').classList.toggle('active');
+    document.querySelector('.pack ').classList.toggle('active');
+    document.querySelector('.pack-one ').classList.toggle('active');
+    document.querySelector('.pack-two ').classList.toggle('active');
+    document.querySelector('.pack-three ').classList.toggle('active');
+    document.querySelector('.two').classList.toggle('active');
+    document.querySelector('.two-one ').classList.toggle('active');
+    document.querySelector('.two-two ').classList.toggle('active');
+    document.querySelector('.two-three ').classList.toggle('active');
+    document.querySelector('.content-one ').classList.toggle('active');
+    document.querySelector('.box-one span').style.color="white";
+    document.querySelector('.box-two span').style.color="white";
+    document.querySelector('.box-three span').style.color="white";
+    document.querySelector('.box-four span').style.color="white";
+    document.querySelector('.box-five span').style.color="white";
+    document.querySelector('.box-six span').style.color="white";
+    document.querySelector('.box-seven span').style.color="white";
+    document.querySelector('.box-eight span').style.color="white";
+
+    document.querySelector('.box-one ').classList.toggle('active');
+    document.querySelector('.box-two ').classList.toggle('active');
+    document.querySelector('.box-three ').classList.toggle('active');
+    document.querySelector('.box-four ').classList.toggle('active');
+    document.querySelector('.box-five ').classList.toggle('active');
+    document.querySelector('.box-six ').classList.toggle('active');
+    document.querySelector('.box-seven ').classList.toggle('active');
+    document.querySelector('.box-eight ').classList.toggle('active');
+    document.querySelector('.box-nine ').classList.toggle('active');
+    document.querySelector('.box-ten ').classList.toggle('active');
+    document.querySelector('.box-eleven ').classList.toggle('active');
+    document.querySelector('.box-twelve ').classList.toggle('active');
+
+
+
+
+};
+function dark(){
+    document.querySelector('.header ').classList.toggle('active');
+    document.querySelector('.body ').classList.toggle('active');
+    document.querySelector('#content ').classList.toggle('active');
+    document.getElementById('light').style.display="block";
+    document.getElementById('dark').style.display="none";
+    document.querySelector('.pack ').classList.toggle('active');
+    document.querySelector('.pack-one ').classList.toggle('active');
+    document.querySelector('.pack-two ').classList.toggle('active');
+    document.querySelector('.pack-three ').classList.toggle('active');
+    document.querySelector('.two').classList.toggle('active');
+    document.querySelector('.two-one ').classList.toggle('active');
+    document.querySelector('.two-two ').classList.toggle('active');
+    document.querySelector('.two-three ').classList.toggle('active');
+    document.querySelector('.content-one ').classList.toggle('active');
+    document.querySelector('.box-one span').style.color="black";
+    document.querySelector('.box-two span').style.color="black";
+    document.querySelector('.box-three span').style.color="black";
+    document.querySelector('.box-four span').style.color="black";
+    document.querySelector('.box-five span').style.color="black";
+    document.querySelector('.box-six span').style.color="black";
+    document.querySelector('.box-seven span').style.color="black";
+    document.querySelector('.box-eight span').style.color="black";
+    document.querySelector('.box-one ').classList.toggle('active');
+    document.querySelector('.box-two ').classList.toggle('active');
+    document.querySelector('.box-three ').classList.toggle('active');
+    document.querySelector('.box-four ').classList.toggle('active');
+    document.querySelector('.box-five ').classList.toggle('active');
+    document.querySelector('.box-six ').classList.toggle('active');
+    document.querySelector('.box-seven ').classList.toggle('active');
+    document.querySelector('.box-eight ').classList.toggle('active');
+    document.querySelector('.box-nine ').classList.toggle('active');
+    document.querySelector('.box-ten ').classList.toggle('active');
+    document.querySelector('.box-eleven ').classList.toggle('active');
+    document.querySelector('.box-twelve ').classList.toggle('active');
+
+
+}
